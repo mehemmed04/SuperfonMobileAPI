7ee74fbc2ae8048118d660fa6d812ecb62527ea3
Qafur Abdullayev <qafurabdullayev05@outlook.com>
2024-08-02 18:47:44 +0400

    sales

---
+++ SuperfonMobileAPI/Controllers/SalesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperfonMobileAPI.Models.Entities;
+using SuperfonMobileAPI.Services;
+using SuperfonWorks.Data;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SuperfonMobileAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        TigerDataService tigerData = null;
+        public SalesController(TigerDataService _tigerData, AppDataService _appDataService)
+        {
+            tigerData = _tigerData;
+        }
+
+        [HttpGet("GetSales")]
+        public async Task<IEnumerable<TigerSales>> GetAllSales()
+        {
+            return await tigerData.GetSales();
+        }
+    }
+}
---
+++ SuperfonMobileAPI/Models/Entities/TigerSales.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperfonMobileAPI.Models.Entities
+{
+    public class TigerSales
+    {
+        public int NR { get; set; }
+        public string SaleLocation { get; set; }
+        public string PlanType { get; set; }
+        public int NumberOfMonth { get; set; }
+        public float SaleAmount { get; set; }
+        public int Goal { get; set; }
+        public string Result { get; set; }
+    }
+}
