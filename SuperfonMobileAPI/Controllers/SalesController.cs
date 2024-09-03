9fe0282ed5055a2bec146120263cde38d74851db
mehemmed <mehemmedbayramov2004@gmail.com>
2024-09-03 11:59:04 +0400

    updated response model

--- SuperfonMobileAPI/Controllers/SalesController.cs
+++ SuperfonMobileAPI/Controllers/SalesController.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using SuperfonMobileAPI.Models.Entities;
 using SuperfonMobileAPI.Services;
-using SuperfonWorks.Data;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -29,14 +26,70 @@ namespace SuperfonMobileAPI.Controllers
         }
 
         [HttpGet("GetSales")]
-        public async Task<IActionResult> GetAllSales()
+        public async Task<IActionResult> GetAllSales(int month)
         {
             if (_memoryCache.TryGetValue(CacheKey, out IEnumerable<TigerSales> salesData))
             {
-                return Ok(salesData);
+                salesData = salesData.Where(s => s.NumberOfMonth == month).ToList();
+                //return Ok(salesData);
+                var response = new List<SaleInfo>();
+                foreach (var sale in salesData)
+                {
+                    bool isExists = false;
+                    foreach (var r in response)
+                    {
+                        if (sale.NR == r.NR)
+                        {
+                            r.Details.Add(new SaleDetail()
+                            {
+                                Goal = sale.Goal,
+                                PlanType = sale.PlanType,
+                                SaleAmount = sale.SaleAmount,
+                                Result = sale.Result
+                            });
+                            isExists = true;
+                        }
+                        else isExists = false;
+                    }
+
+                    if (!isExists)
+                        response.Add(new SaleInfo()
+                        {
+                            NR = sale.NR,
+                            SaleLocation = sale.SaleLocation,
+                            Details = new List<SaleDetail>()
+                            {
+                                new SaleDetail()
+                                {
+                                    Goal = sale.Goal,
+                                    PlanType = sale.PlanType,
+                                    SaleAmount = sale.SaleAmount,
+                                    Result = sale.Result
+                                }
+                            }
+                    });
+                }
+                
+                return Ok(response);
             }
 
             return BadRequest("There is not any sale");
         }
+        public class SaleDetail
+        {
+            public string PlanType { get; set; }
+            public float SaleAmount { get; set; }
+            public decimal Goal { get; set; }
+            public string Result { get; set; }
+        }
+
+        public class SaleInfo
+        {
+            public int NR { get; set; }
+            public string SaleLocation { get; set; }
+
+            public List<SaleDetail> Details { get; set; } = new List<SaleDetail>();
+        }
+
     }
 }
